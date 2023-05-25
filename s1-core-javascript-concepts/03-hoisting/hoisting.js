b527ee90204ac199ae07eaa98ff6d6b8f047cc69
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 12:35:24 +0200

    added 03-hoisting challenge

---
+++ s1-core-javascript-concepts/03-hoisting/hoisting.js
@@ -0,0 +1,15 @@
+// What will be console.logged here
+//
+// Question 1
+// console.log(foo);
+// foo = 1;
+
+// question 2
+// console.log(foo);
+// var foo = 2;
+//
+
+// question 3
+// foo = 3;
+// console.log(foo);
+// var foo;

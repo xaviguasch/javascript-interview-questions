316b7c0819b2eee3cda4247703e12d3f62a9ec65
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 12:34:12 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/02-difference-between-null-and-undefined/difference-between-null-and-undefined.js
+++ s1-core-javascript-concepts/02-difference-between-null-and-undefined/difference-between-null-and-undefined.js
@@ -1,9 +1,9 @@
 // What will be logged in first example and in second example?
 //
 let var1
-console.log(var1)
-console.log(typeof var1)
+console.log(var1) // undefined
+console.log(typeof var1) // undefined
 
 let var2 = null
-console.log(var2)
-console.log(typeof var2)
+console.log(var2) // null
+console.log(typeof var2) // object

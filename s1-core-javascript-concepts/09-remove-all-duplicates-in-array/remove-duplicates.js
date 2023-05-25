207af15e19b34f45c5fb2f56c72ec699da6eb447
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 21:03:24 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/09-remove-all-duplicates-in-array/remove-duplicates.js
+++ s1-core-javascript-concepts/09-remove-all-duplicates-in-array/remove-duplicates.js
@@ -1,3 +1,25 @@
 // Remove all duplicates in the array
 
 const array = [2, 4, 4, 3, 5, 3]
+
+// const removeDuplicates = (arr) => [...new Set(arr)]
+
+// const removeDuplicates = (arr) => {
+//   const uniqueArr = []
+
+//   for (const el of arr) {
+//     if (!uniqueArr.includes(el)) {
+//       uniqueArr.push(el)
+//     }
+//   }
+
+//   return uniqueArr
+// }
+
+const removeDuplicates = (arr) => {
+  return arr.reduce((acc, el) => {
+    return acc.includes(el) ? acc : [...acc, el]
+  }, [])
+}
+
+console.log(removeDuplicates(array))

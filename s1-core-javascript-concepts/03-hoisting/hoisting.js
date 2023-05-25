1205a5fa568b024a36e855883aa422a7abf7b42c
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 12:44:47 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/03-hoisting/hoisting.js
+++ s1-core-javascript-concepts/03-hoisting/hoisting.js
@@ -1,15 +1,20 @@
 // What will be console.logged here
 //
 // Question 1
-// console.log(foo);
-// foo = 1;
+// console.log(foo)
+// foo = 1
+
+// reference error: foo is not defined
 
 // question 2
-// console.log(foo);
-// var foo = 2;
-//
+// console.log(foo)
+// var foo = 2
+
+// undefined, due to hosting
 
 // question 3
-// foo = 3;
-// console.log(foo);
-// var foo;
+foo = 3
+console.log(foo)
+var foo
+
+// 3, due to hosting

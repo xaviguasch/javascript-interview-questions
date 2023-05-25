851fe5cd111ac04ffca461490bbb880b9396474f
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 18:38:46 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/06-adding-elements-to-an-array/adding-elements-to-an-array.js
+++ s1-core-javascript-concepts/06-adding-elements-to-an-array/adding-elements-to-an-array.js
@@ -1,1 +1,7 @@
 // Write a function which get's an array and an element and returns a array with this element at the end
+
+const makeNewArr = (arr, el) => {
+  return [...arr, el]
+}
+
+console.log(makeNewArr(['a', 'b', 'c'], 'xxxx'))

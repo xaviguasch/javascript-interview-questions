afd2a9a81266ec2bac884b2789783df0f723efd6
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 12:58:06 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/04-closures/closures.js
+++ s1-core-javascript-concepts/04-closures/closures.js
@@ -1,1 +1,23 @@
 // Create a counter function which has increment and getValue functionality
+
+const privateCounter = () => {
+  let count = 0
+
+  return {
+    increment: (val = 1) => {
+      count += val
+    },
+    getValue: () => {
+      return count
+    },
+  }
+}
+
+const counter = privateCounter()
+
+console.log(counter.getValue())
+counter.increment()
+console.log(counter.getValue())
+counter.increment()
+
+console.log(counter.getValue())

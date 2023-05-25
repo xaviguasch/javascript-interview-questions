333b43c1116b3cf12af79b88d3be7f3af7bfe55e
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 11:45:20 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/01-mapping-users-to-get-usernames/main.js
+++ s1-core-javascript-concepts/01-mapping-users-to-get-usernames/main.js
@@ -4,20 +4,38 @@
 const users = [
   {
     id: 1,
-    name: "Jack",
+    name: 'Jack',
     isActive: true,
     age: 20,
   },
   {
     id: 2,
-    name: "John",
+    name: 'John',
     isActive: true,
     age: 18,
   },
   {
     id: 3,
-    name: "Mike",
+    name: 'Mike',
     isActive: false,
     age: 30,
   },
-];
+]
+
+// 1)-
+
+const userNames = users.map((user) => user.name)
+
+console.log(userNames)
+
+// 2)-
+
+const activeUsers = users.filter((user) => user.isActive).map((acUser) => acUser.name)
+
+console.log(activeUsers)
+
+// 3)-
+
+const sortedUsers = users.sort((a, b) => b.age - a.age)
+
+console.log(sortedUsers)

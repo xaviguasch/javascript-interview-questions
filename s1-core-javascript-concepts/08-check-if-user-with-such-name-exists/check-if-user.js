6683f0790b9ada700b534d763e5a8ba7c458bf42
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 19:00:11 +0200

    added solution to previous challenge

---
+++ s1-core-javascript-concepts/08-check-if-user-with-such-name-exists/check-if-user.js
@@ -0,0 +1,19 @@
+// Check that user with such name exists in array of objects
+
+const users = [
+  {
+    id: 1,
+    name: 'Jack',
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: 'John',
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: 'Mike',
+    isActive: false,
+  },
+]

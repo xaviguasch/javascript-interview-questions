b8b127938feef3f6097ff10f701d7240b5444620
Xavi Guasch <xaviguasch@gmail.com>
2023-05-25 18:02:49 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/05-currying/currying.js
+++ s1-core-javascript-concepts/05-currying/currying.js
@@ -1,3 +1,7 @@
 // Write a function which helps to achieve multiply(a)(b) and returns product of a and b
 // Create a curry function
 // What can your curried function do?
+
+const multiply = (num1) => (num2) => num1 * num2
+
+console.log(multiply(5)(4))

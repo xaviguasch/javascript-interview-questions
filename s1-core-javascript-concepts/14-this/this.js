ac5cb1024aafd2389447c01c5f9b475aa2d042e8
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 20:14:18 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/14-this/this.js
+++ s1-core-javascript-concepts/14-this/this.js
@@ -7,26 +7,33 @@
 
 // getItem();
 
+// We will see the global object (in this case, the window)
+
 // Task 2
 // const item = {
-//   title: "Ball",
+//   title: 'Ball',
 //   getItem() {
-//     console.log("this", this);
+//     console.log('this', this)
 //   },
-// };
-
-// item.getItem();
-
-// Task 3
-// class Item {
-//   title = "Ball";
-//   getItem() {
-//     function someFn() {
-//       console.log("this", this);
-//     }
-//     someFn();
-//   }
 // }
 
-// const item = new Item();
-// item.getItem();
+// item.getItem()
+
+// We will see the object item
+
+// Task 3
+class Item {
+  title = 'Ball'
+  getItem() {
+    console.log('this', this)
+    function someFn() {
+      console.log('this', this)
+    }
+    someFn()
+  }
+}
+
+const item = new Item()
+item.getItem()
+
+// the first "this" will give us the instance of Item. The second "this" will print 'undefined' because inside a function we're referencing the global object. We should use an arrow function instead so the "this" would get the context of the parent.

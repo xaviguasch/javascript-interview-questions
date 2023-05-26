5887f9ae085039ec6833aaed165efbd95d75c560
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 19:46:59 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/12-writing-shuffle-function/shuffle-function.js
+++ s1-core-javascript-concepts/12-writing-shuffle-function/shuffle-function.js
@@ -1,7 +1,10 @@
 // write a function which implements shuffle
 
 const shuffleItems = (items) => {
-  return
+  return items
+    .map((item) => ({ sort: Math.random(), value: item }))
+    .sort((item1, item2) => item1.sort - item2.sort)
+    .map((item) => item.value)
 }
 
-console.log(shuffleItems())
+console.log(shuffleItems([1, 2, 3, 4, 5, 6]))

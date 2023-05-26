afa981efbf7fb582bb6e3b9882d7e7fe310f2ded
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 18:47:06 +0200

    added solution to previous challenge

--- s1-core-javascript-concepts/11-writing-range-function/writing-range-function.js
+++ s1-core-javascript-concepts/11-writing-range-function/writing-range-function.js
@@ -1,1 +1,22 @@
 // Write a function which implements range
+
+// range (1,50)
+// 1,2,3,4,5...50
+
+const range = (start, end) => {
+  const arr = []
+
+  for (let i = start; i <= end; i++) {
+    arr.push(i)
+  }
+
+  return arr
+}
+
+// console.log(range(1, 50))
+
+const rangeAdvanced = (start, end) => {
+  return [...Array(end).keys()].map((el) => el + start)
+}
+
+console.log(rangeAdvanced(1, 50))

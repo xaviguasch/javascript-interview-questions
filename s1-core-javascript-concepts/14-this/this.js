e4bc9a0bf8542ed7f3ae124c6c1d86a4c7212bf8
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 20:03:41 +0200

    added 14-this exercise

---
+++ s1-core-javascript-concepts/14-this/this.js
@@ -0,0 +1,32 @@
+// What will be logged here?
+
+// Task 1
+// function getItem() {
+//   console.log(this);
+// }
+
+// getItem();
+
+// Task 2
+// const item = {
+//   title: "Ball",
+//   getItem() {
+//     console.log("this", this);
+//   },
+// };
+
+// item.getItem();
+
+// Task 3
+// class Item {
+//   title = "Ball";
+//   getItem() {
+//     function someFn() {
+//       console.log("this", this);
+//     }
+//     someFn();
+//   }
+// }
+
+// const item = new Item();
+// item.getItem();

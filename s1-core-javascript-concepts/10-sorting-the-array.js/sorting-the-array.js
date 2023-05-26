07c3eaa718112e5bacc174664ece915b740d99ea
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 18:22:03 +0200

    added 2nd part of previous exercise

--- s1-core-javascript-concepts/10-sorting-the-array.js/sorting-the-array.js
+++ s1-core-javascript-concepts/10-sorting-the-array.js/sorting-the-array.js
@@ -7,3 +7,11 @@ const sortArrayFunc = (arr) => {
 }
 
 console.log(sortArrayFunc(numsArr))
+
+// Sort array of objects by author's lastname
+
+const books = [
+  { name: 'Harry Potter', author: 'Joanne Rowling' },
+  { name: 'Warcross', author: 'Marie Lu' },
+  { name: 'The Hunger Games', author: 'Suzanne Collins' },
+]

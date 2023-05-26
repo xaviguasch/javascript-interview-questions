4526d1d2448f57a40e30627108b7c296814f92d8
Xavi Guasch <xaviguasch@gmail.com>
2023-05-26 19:58:47 +0200

    solved previous challenge

--- s1-core-javascript-concepts/13-find-the-number-of-occurrences-of-min-value/find-number-occurrences.js
+++ s1-core-javascript-concepts/13-find-the-number-of-occurrences-of-min-value/find-number-occurrences.js
@@ -1,3 +1,25 @@
 // Find the number of occurences of minimum value in the list
 
-const arr = [1, 2, 3, 1, 1]
+const array = [1, 2, 3, 1, 1]
+
+const findNumber = (arr) => {
+  const hashMap = {}
+
+  let min = arr[0]
+
+  for (const el of arr) {
+    if (hashMap[el]) {
+      hashMap[el]++
+    } else {
+      hashMap[el] = 1
+    }
+
+    if (el < min) {
+      min = el
+    }
+  }
+
+  return `The lowest number is ${min} and it gets repeated ${hashMap[min]} times`
+}
+
+console.log(findNumber(array))

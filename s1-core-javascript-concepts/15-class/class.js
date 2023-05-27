270a6108c72ca189cdb7b18555b1d97ce2fc4996
Xavi Guasch <xaviguasch@gmail.com>
2023-05-27 17:38:24 +0200

    added solution to first part of previous challenge

--- s1-core-javascript-concepts/15-class/class.js
+++ s1-core-javascript-concepts/15-class/class.js
@@ -1,3 +1,37 @@
 // Design a class for employee which takes id and name in during construction of object and has a salary property
 
+class Employee {
+  constructor(id, name) {
+    if (!id || !name) {
+      throw new Error('Employee id and name are mandatory')
+    }
+
+    this.id = id
+    this.name = name
+  }
+
+  setSalary(salary) {
+    this.salary = salary
+  }
+
+  getId() {
+    return this.id
+  }
+
+  getName() {
+    return this.name
+  }
+
+  getSalary() {
+    return this.salary
+  }
+}
+
+const michaelEmployee = new Employee('45e', 'Michael')
+
+console.log(michaelEmployee)
+michaelEmployee.setSalary(5000)
+console.log(michaelEmployee.getSalary())
+console.log(michaelEmployee.getName())
+
 // Design a class for manager which is employee and can have department property

421141887fbed9537dfa2a68df032c9f5d732667
Xavi Guasch <xaviguasch@gmail.com>
2023-05-27 17:02:58 +0200

    added 15-class exercise

---
+++ s1-core-javascript-concepts/15-class/class.js
@@ -0,0 +1,3 @@
+// Design a class for employee which takes id and name in during construction of object and has a salary property
+
+// Design a class for manager which is employee and can have department property
